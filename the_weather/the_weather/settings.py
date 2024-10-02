62a95c5f6cc3d996b50f972f2d36179a2a04a3a3
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-10-02 04:18:11 +0000

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS in settings.py

--- the_weather/the_weather/settings.py
+++ the_weather/the_weather/settings.py
@@ -34,9 +34,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-f_*d)epf0$&v-w
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Astrokojo.pythonanywhere.com', '127.0.0.1']
 
-
+CSRF_TRUSTED_ORIGINS = ['https://Astrokojo.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
